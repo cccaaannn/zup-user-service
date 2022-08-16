a994f4de0806e7c1448dda66281d2bd287936420
cccaaannn <can.kurt.aa@gmail.com>
2022-08-17 00:30:43 +0300

    dtos

---
+++ src/main/java/com/can/zupuserservice/core/data/dto/AccessToken.java
@@ -0,0 +1,12 @@
+package com.can.zupuserservice.core.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccessToken {
+    private String token;
+}
