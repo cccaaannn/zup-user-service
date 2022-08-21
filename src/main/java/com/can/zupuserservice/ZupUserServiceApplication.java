52382cc44548106480be99099186996e366cfaef
cccaaannn <can.kurt.aa@gmail.com>
2022-08-21 05:38:35 +0300

    app

---
+++ src/main/java/com/can/zupuserservice/ZupUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.can.zupuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZupUserServiceApplication.class, args);
+    }
+
+}
