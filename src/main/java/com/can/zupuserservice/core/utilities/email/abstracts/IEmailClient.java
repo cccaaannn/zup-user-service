1fbf2161f5abc42e986ed49e1a221126f33a6385
cccaaannn <can.kurt.aa@gmail.com>
2022-08-21 05:39:16 +0300

    core email

---
+++ src/main/java/com/can/zupuserservice/core/utilities/email/abstracts/IEmailClient.java
@@ -0,0 +1,15 @@
+package com.can.zupuserservice.core.utilities.email.abstracts;
+
+import javax.mail.MessagingException;
+import javax.security.auth.login.CredentialNotFoundException;
+import java.util.List;
+
+public interface IEmailClient {
+    void send(String username, String password, List<String> toList, String messageSubject, String messageBody) throws MessagingException;
+
+    void send(String username, String password, String to, String messageSubject, String messageBody) throws MessagingException;
+
+    void send(List<String> toList, String messageSubject, String messageBody) throws CredentialNotFoundException, MessagingException;
+
+    void send(String to, String messageSubject, String messageBody) throws CredentialNotFoundException, MessagingException;
+}
---
+++ src/main/java/com/can/zupuserservice/core/utilities/email/concretes/GmailClient.java
@@ -0,0 +1,15 @@
+package com.can.zupuserservice.core.utilities.email.concretes;
+
+import com.can.zupuserservice.core.utilities.email.abstracts.IEmailClient;
+
+public class GmailClient extends EmailClient implements IEmailClient {
+
+    public GmailClient() {
+        super("smtp.gmail.com", "465");
+    }
+
+    public GmailClient(String username, String password) {
+        super("smtp.gmail.com", "465", username, password);
+    }
+
+}
