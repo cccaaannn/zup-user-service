f5483c579160ad954940d5999e5c8376d4f18a73
cccaaannn <can.kurt.aa@gmail.com>
2022-08-30 17:43:29 +0300

    enums

---
+++ src/main/java/com/can/zupuserservice/data/enums/DefaultRoles.java
@@ -0,0 +1,14 @@
+package com.can.zupuserservice.data.enums;
+
+public enum DefaultRoles {
+    USER("USER", "Default user role"),
+    ADMIN("ADMIN", "Admin role"),
+    SYS_ADMIN("SYS_ADMIN", "Sys admin role");
+
+    public final String name;
+    public final String description;
+    DefaultRoles(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/can/zupuserservice/data/enums/TokenType.java
@@ -0,0 +1,12 @@
+package com.can.zupuserservice.data.enums;
+
+public enum TokenType {
+    AUTHENTICATION(0),
+    ACCOUNT_ACTIVATION(1),
+    PASSWORD_RESET(2);
+
+    public final int type;
+    TokenType(int type) {
+        this.type = type;
+    }
+}
