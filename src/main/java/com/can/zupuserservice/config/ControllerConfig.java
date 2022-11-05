d939a4a1d75a990fcec07538067ee21bab46e200
cccaaannn <can.kurt.aa@gmail.com>
2022-11-05 05:04:48 +0300

    validations added and more errors

--- src/main/java/com/can/zupuserservice/config/ControllerConfig.java
+++ src/main/java/com/can/zupuserservice/config/ControllerConfig.java
@@ -5,15 +5,20 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.can.zupuserservice.core.exception.ForbiddenException;
 import com.can.zupuserservice.core.exception.NotFoundException;
 import com.can.zupuserservice.core.utilities.result.concretes.ErrorResult;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConversionException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,21 +35,42 @@ public class ControllerConfig {
     @ResponseStatus(value = HttpStatus.FORBIDDEN)
     @ExceptionHandler(ForbiddenException.class)
     public ResponseEntity<Object> notAuthorizedException(ForbiddenException ex) {
-        ex.printStackTrace();
+//        ex.printStackTrace();
         return new ResponseEntity<>(new ErrorResult(ex.getMessage()), new HttpHeaders(), HttpStatus.FORBIDDEN);
     }
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Object> notFoundException(NotFoundException ex) {
-        ex.printStackTrace();
+//        ex.printStackTrace();
         return new ResponseEntity<>(new ErrorResult(ex.getMessage()), new HttpHeaders(), HttpStatus.NOT_FOUND);
+    }
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({TypeMismatchException.class, MethodArgumentTypeMismatchException.class})
+    public ResponseEntity<Object> notFoundException(TypeMismatchException ex) {
+//        ex.printStackTrace();
+        return new ResponseEntity<>(new ErrorResult("There is a type mismatch"), new HttpHeaders(), HttpStatus.NOT_FOUND);
+    }
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageConversionException.class)
+    public ResponseEntity<Object> notFoundException(HttpMessageConversionException ex) {
+//        ex.printStackTrace();
+        return new ResponseEntity<>(new ErrorResult("Provided http method is not supported"), new HttpHeaders(), HttpStatus.NOT_FOUND);
+    }
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({ValidationException.class, ConstraintViolationException.class})
+    public ResponseEntity<Object> notFoundException(ValidationException ex) {
+//        ex.printStackTrace();
+        return new ResponseEntity<>(new ErrorResult("Parameter validation failed"), new HttpHeaders(), HttpStatus.NOT_FOUND);
     }
 
     @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(value = {JWTVerificationException.class, JWTDecodeException.class})
     public ResponseEntity<Object> jwtVerificationException(JWTVerificationException ex) {
-        ex.printStackTrace();
+//        ex.printStackTrace();
         return new ResponseEntity<>(new ErrorResult("Not authorized"), new HttpHeaders(), HttpStatus.UNAUTHORIZED);
     }
 
