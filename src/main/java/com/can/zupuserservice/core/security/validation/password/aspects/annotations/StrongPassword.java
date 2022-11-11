4ce29158f7c0c03eea4d91eba94062eba02138e8
cccaaannn <can.kurt.aa@gmail.com>
2022-11-12 00:27:59 +0300

    password validator annotation rewritten

--- src/main/java/com/can/zupuserservice/core/security/validation/password/aspects/annotations/StrongPassword.java
+++ src/main/java/com/can/zupuserservice/core/security/validation/password/aspects/annotations/StrongPassword.java
@@ -1,29 +1,49 @@
-package com.can.zupuserservice.core.aspects.annotations;
+package com.can.zupuserservice.core.security.validation.password.aspects.annotations;
 
-import com.can.zupuserservice.core.aspects.concretes.StrongPasswordValidator;
+import com.can.zupuserservice.core.security.validation.password.aspects.concretes.StrongPasswordValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+/**
+ * <h2>Password validation annotation.</h2>
+ * <hr/><br/>
+ * Uses Custom <i>PasswordValidator</i> to validate passwords.
+ * See <i>PasswordValidator</i> implementation for more detail.
+ *
+ * @author Can Kurt
+ * @version 1.0
+ * @since 2022-11-11
+ */
+@Documented
 @Constraint(validatedBy = StrongPasswordValidator.class)
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface StrongPassword {
     String message() default "Password is not strong enough";
 
-    boolean detailedMessages() default true;
+    boolean usePropertyKeyOnMessages() default false;
+
+    boolean detailedDefaultMessages() default true;
+
     int minLength() default 8;
+
     int maxLength() default 30;
+
     int minLowerCaseLetterCount() default 1;
+
     int minUpperCaseLetterCount() default 1;
+
     int minDigitCount() default 1;
+
     int minSpecialCharCount() default 1;
+
     boolean checkForBadChars() default true;
 
+    boolean ignoreCase() default false;
+
     Class<?>[] groups() default {};
+
     Class<? extends Payload>[] payload() default {};
 }
---
+++ src/main/java/com/can/zupuserservice/core/security/validation/password/data/dtos/PasswordValidationResult.java
@@ -0,0 +1,16 @@
+package com.can.zupuserservice.core.security.validation.password.data.dtos;
+
+import com.can.zupuserservice.core.security.validation.password.data.enums.PasswordConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PasswordValidationResult {
+    private Boolean status;
+    private List<PasswordConstraint> passwordConstraints;
+}
